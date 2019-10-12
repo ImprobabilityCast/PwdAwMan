418f474f40d87f2ed0274882e9678bdcf75dfc1b
ImprobabilityCast <segmentationfault3@outlook.com>
2019-10-12 15:46:29 -0400

    command parsing

--- PwdAwMan.java
+++ PwdAwMan.java
@@ -1,6 +1,7 @@
 import java.io.Console;
 import java.io.File;
 import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -19,9 +20,8 @@ public class PwdAwMan {
     private static final int USERNAME = 3;
     private static final int PASSWORD = 4;
     private static final int NOTE = 5;
-    private static final int NUM_COLS = 5;
-    private static SortedMap<String, String[]> table
-            = new TreeMap<String, String[]>();
+    private static List<List<String>> table
+            = new LinkedList<List<String>>();
 
     private PwdAwMan() {
     }
@@ -48,30 +48,36 @@ public class PwdAwMan {
         return ret;
     }
 
-    private static String getLastCell(String line, int subStrLen) {
-        int i = subStrLen - 1;
-        char c = line.charAt(i);
+    private static String getNextToken(String line, char sep, int start) {
+        int end = start;
         int quotes = 0;
 
-        while ((c != ',' || quotes % 2 != 0) && i - 1 > 0) {
-            if (c == '"') {
+         while (end < line.length()
+                && (line.charAt(end) != sep || quotes % 2 != 0)) {
+            if (line.charAt(end) == '"') {
                 quotes++;
             }
-            i--;
-            c = line.charAt(i);
+            end++;
         }
-        return line.substring(i + 1, subStrLen);
+
+        return line.substring(start, end);
+    }
+
+    private static List<String> splitWithQuotes(String line, char sep) {
+        List<String> list  = new ArrayList<>();
+        int pos = 0;
+        while (pos < line.length()) {
+            String token = getNextToken(line, sep, pos);
+            // +1 to skip sep char
+            pos += token.length() + 1;
+            list.add(token);
+        }
+        return list;
     }
 
     private static void parseCSV(String csv) {
         for (String s : csv.split("\n|\r\n")) {
-            String note = getLastCell(s, s.length());
-            // -1 to skip commas
-            int end = s.length() - (note.length() + 1);
-            String pwd = getLastCell(s, end);
-            end -= (pwd.length() + 1);
-            String[] val = {pwd, note};
-            table.put(s.substring(0, end - 1), val);
+            PwdAwMan.table.add(splitWithQuotes(s, ','));
         }
     }
 
@@ -82,27 +88,58 @@ public class PwdAwMan {
         enc.setPassword(pwd);
     }
 
+    private static void printHeaders() {
+        System.out.println("ID\tPLACE\t\tTYPE\tURL\t\tUSERNAME");
+    }
+
+    private static String fmtRow(List<String> row, int rowNum) {
+        StringBuilder sb = new StringBuilder(80);
+        sb.append(rowNum);
+        sb.append('\t');
+        sb.append(row.get(PLACE));
+        sb.append('\t');
+        sb.append(row.get(TYPE));
+        sb.append('\t');
+        sb.append(row.get(URL));
+        sb.append('\t');
+        sb.append(row.get(USERNAME));
+        return sb.toString();
+    }
+
     private static void prompt() {
         Scanner sc = new Scanner(System.in);
         while (true) {
+            System.out.print("> ");
             try {
-                String[] cmd = sc.nextLine().split(" ");
-                if (cmd[0].equals("list")) {
-                    for (String key : PwdAwMan.table.keySet()) {
-                        System.out.println(key);
+                List<String> cmd = splitWithQuotes(sc.nextLine(), ' ');
+                System.out.println(":" + cmd.toString());
+                if (cmd.get(0).equals("list")) {
+                    printHeaders();
+                    for (int i = 0; i < table.size(); i++) {
+                        System.out.println(fmtRow(table.get(i), i));
                     }
-                } else if (cmd[0].equals("replace")) {
+                } else if (cmd.get(0).equals("replace")) {
                     
-                } else if (cmd[0].equals("show")) {
+                } else if (cmd.get(0).equals("show")) {
 
-                } else if (cmd[0].equals("copy")) {
+                } else if (cmd.get(0).equals("copy")) {
                     
-                } else if (cmd[0].equals("add")) {
-                    //place type url username pwd note
+                } else if (cmd.get(0).equals("add")) {
+                    //id place type url username pwd note
                     
                 }
             } catch (Exception e) {
-
+                System.out.println("Command syntax:\n"
+                    + "list\n"
+                    + "replace <id> TYPE <replacement>\n"
+                    + "show <id> TYPE\n"
+                    + "copy <id> TYPE\n"
+                    + "add <place> <type> <url> <username> <password> <note>\n"
+                    + "Where TYPE is one of: <all|place|type|url|username|password|note>\n"
+                    + "Omiting arguments from add will keep those fields blank.\n"
+                    + "If the 'all' option is selected, then <replacement> has the same\n"
+                    + "syntax as add."
+                );
             }
         }
     }
