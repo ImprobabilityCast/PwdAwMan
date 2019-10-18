a25bc7e8a18ad05b36361c304b49d47ba7bda609
ImprobabilityCast <segmentationfault3@outlook.com>
2019-10-18 01:33:53 -0400

    fix csv parser

--- improbabilitycast/pwdawman/ParseUtil.java
+++ improbabilitycast/pwdawman/ParseUtil.java
@@ -25,7 +25,7 @@ public class ParseUtil {
     public static List<String> splitWithQuotes(String line, char sep) {
         List<String> list  = new ArrayList<>();
         int pos = 0;
-        while (pos < line.length()) {
+        while (pos <= line.length()) {
             String token = getNextToken(line, sep, pos);
             // +1 to skip sep char
             pos += token.length() + 1;
@@ -34,10 +34,38 @@ public class ParseUtil {
         return list;
     }
 
+    private static String csvUnEscape(String s) {
+        StringBuilder sb = new StringBuilder(s);
+        boolean slash = false;
+
+        if (sb.length() > 0 && sb.charAt(0) == '"'
+                && sb.charAt(sb.length() - 1) == '"') {
+            sb.deleteCharAt(sb.length() - 1);
+            sb.deleteCharAt(0);
+        }
+
+        for (int i = 0; i < sb.length(); i++) {
+            char c = sb.charAt(i);
+            if (slash && c == '"') {
+                i--;
+                sb.deleteCharAt(i);
+            }
+            slash = (c == '\\');
+        }
+        return sb.toString();
+    }
+
+    private static void csvUnEscapeRow(List<String> row) {
+        for (int i = 0; i < row.size(); i++) {
+            row.set(i, csvUnEscape(row.get(i)));
+        }
+    }
+
     public static void parseCSV(List<List<String>> table, String csv) {
         table.clear();
         for (String s : csv.split("\n|\r\n")) {
             List<String> row = splitWithQuotes(s, ',');
+            csvUnEscapeRow(row);
             row.add(0, String.valueOf(table.size()));
             DisplayUtil.updatePadding(row);
             row.remove(0);
@@ -46,11 +74,23 @@ public class ParseUtil {
     }
 
     private static String csvEscape(String s) {
-        s = s.replaceAll("\"", "\\\"");
-        if (s.contains(",")) {
-            s = "\"" + s + "\"";
+        // add 4 so sb will probably not have to realloc
+        StringBuilder sb = new StringBuilder(s.length() + 4);
+        sb.append('\"');
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '"') {
+                sb.append('\\');
+            }
+            sb.append(c);
         }
-        return s;
+        sb.append('\"');
+
+        if (s.contains(",")) {
+            return sb.toString();
+        } else {
+            return sb.substring(1, sb.length() - 1);
+        }
     }
 
     // every list in the list must have size > 0
@@ -60,7 +100,7 @@ public class ParseUtil {
 
         for (int i = 0; i < table.size(); i++) {
             List<String> line = table.get(i);
-            String escaped = csvEscape(line.get(i));
+            String escaped = csvEscape(line.get(0));
             sb.append(escaped);
             for (int j = 1; j < line.size(); j++) {
                 sb.append(',');
